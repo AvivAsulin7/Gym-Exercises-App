04b5a55334d6610c1dcee7c1404cb9c00412767b
אביב אסולין <avivasulin1211@gmail.com>
2022-09-05 13:48:29 +0300

    First commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+function Home() {
+  return (
+    <Box>
+      {/* <HeroBanner />
+      <SearchExercises />
+      <Exercises /> */}
+    </Box>
+  );
+}
+
+export default Home;
